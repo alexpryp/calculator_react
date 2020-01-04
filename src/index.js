e1570b6d42cd7b9e654a30501fbbadfc0002cce7
alexpryp <alexpryp@gmail.com>
2020-01-05 01:36:37 +0200

    Calculator layout created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Calculator() {
+    return (
+        <div className="calculator">
+            <div className="display">0</div>
+            <div className="buttons-panel">
+                <div className="buttons-row">
+                    <button className="button-blue">C</button>
+                    <button className="dark-grey">&#247;</button>
+                    <button className="dark-grey">&#215;</button>
+                    <button className="dark-grey"><i className="fa fa-backspace"></i></button>
+                </div>
+                <div className="buttons-row">
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button className="dark-grey">-</button>
+                </div>
+                <div className="buttons-row">
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button className="dark-grey">+</button>
+                </div>
+                <div className="buttons-row">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button className="dark-grey">( )</button>
+                </div>
+                <div className="buttons-row">
+                    <button>0</button>
+                    <button>.</button>
+                    <button>&#177;</button>
+                    <button className="button-orange">=</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
